4c73805e458b5f80520d6ac7eaea089a0c3de8a1
Freddy Mande <16734663+FreddKM@users.noreply.github.com>
2026-07-10 21:15:16 -0700

    chore: upgrade to Angular 20

--- src/app/services/translated-title.strategy.ts
+++ src/app/services/translated-title.strategy.ts
@@ -1,5 +1,5 @@
-import { DOCUMENT } from '@angular/common';
-import { Inject, Injectable } from '@angular/core';
+
+import { Inject, Injectable, DOCUMENT } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
